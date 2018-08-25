afca6d3f4fdb8f39ce7174f37906f8bb111df4bb
matus <bartko.mat@gmail.com>
2018-08-26 01:22:52 +0200

    path change attemp 3

--- src/main/java/sk/matusko/ecs/MainController.java
+++ src/main/java/sk/matusko/ecs/MainController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/")
     public String home() {
         return "Hello Docker World, i have added webhooks";
+    }
+
+    @RequestMapping(value = "/healthcheck")
+    public void healthcheck() {
+        return;
     }
 }
