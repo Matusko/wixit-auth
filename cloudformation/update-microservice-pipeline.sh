2d23c309e0291468f74c3f0ce04a74d057c52024
matus <bartko.mat@gmail.com>
2018-09-22 13:14:20 +0200

    cloudformation moved to another repo

--- cloudformation/update-microservice-pipeline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-aws cloudformation update-stack \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --region eu-west-1 \
-    --stack-name wixit-test-auth-pipeline \
-    --template-body file://microservice-pipeline.yaml \
-    --parameters file://microservice-pipeline-params.json
