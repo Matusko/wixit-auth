4af61d0008877dbd464cba1c5efc4af77c28afac
matus <bartko.mat@gmail.com>
2018-09-23 21:46:42 +0200

    no pswd provided debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "ENTRYPOINT ENTRYPOINT ENTRYPOINT ENTRYPOINT"
+echo ${CONTEXT_PATH}
+echo ${DB_ENDPOINT}
+echo ${DB_PORT}
+echo ${DB_MASTER_NAME}
+echo ${DB_MASTER_PSWD}
+
 CONTEXT_PATH_ESCAPED=$(echo ${CONTEXT_PATH} | sed 's/\//\\\//g')
 sed -i \
     -e "s/{{% DB_ENDPOINT %}}/${DB_ENDPOINT}/" \
@@ -9,6 +16,6 @@ sed -i \
     -e "s/{{% CONTEXT_PATH %}}/$CONTEXT_PATH_ESCAPED/" \
     conf.yml
 
-
+cat conf.yml
 java -Djava.security.egd=file:/dev/./urandom -jar /app.jar --spring.config.location=classpath:application.yml,file:./conf.yml
 exec "$@"
