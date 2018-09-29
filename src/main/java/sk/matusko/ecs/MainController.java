08233781416e97f2c2179b5059abc0e6f41949a8
matus <bartko.mat@gmail.com>
2018-09-29 23:06:27 +0200

    test change

--- src/main/java/sk/matusko/ecs/MainController.java
+++ src/main/java/sk/matusko/ecs/MainController.java
@@ -8,7 +8,7 @@ public class MainController {
 
     @RequestMapping(value = "/")
     public String home() {
-        return "Hello Docker World, i changed have added webhooks in seprate repo with logger another test";
+        return "Hello Docker World, i changed have added asa sa sa webhooks in seprate repo with logger another test";
     }
 
     @RequestMapping(value = "/healthcheck")
