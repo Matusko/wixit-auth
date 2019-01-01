a48dd6b544163f4c1ac5715eb09daa148675f929
matus <bartko.mat@gmail.com>
2019-01-01 21:36:04 +0100

    redis ssl and auth removed

--- dev/redis/README.md
+++ dev/redis/README.md
@@ -1,5 +1,3 @@
     docker build -t "matusko/redis:latest" .
-    
-insert correct password $PASSWORD
-    
-    docker run -e PASSWORD='$PASSWORD' -p 6379:6379 -t matusko/redis
+        
+    docker run -p 6379:6379 -t matusko/redis
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,6 @@ sed -i \
     -e "s/{{% DB_MASTER_PSWD %}}/${DB_MASTER_PSWD}/" \
     -e "s/{{% REDIS_ENDPOINT %}}/${REDIS_ENDPOINT}/" \
     -e "s/{{% REDIS_PORT %}}/${REDIS_PORT}/" \
-    -e "s/{{% REDIS_PSWD %}}/${REDIS_PSWD}/" \
-    -e "s/{{% REDIS_SSL_ENABLED %}}/$REDIS_SSL_ENABLED/" \
     -e "s/{{% CONTEXT_PATH %}}/$CONTEXT_PATH_ESCAPED/" \
     conf.yml
 
